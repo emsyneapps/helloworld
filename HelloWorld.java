5814cee8621bd0c963d0ab3bdc6c1b79d1e17ff2
emsyneapps <60918767+emsyneapps@users.noreply.github.com>
2022-09-19 11:31:02 +0530

    chnage 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Vikek from Java");
 		System.out.println("Hello Vikek from android");
+		System.out.println("Hello Vikek from android2");
 
 
 	}
